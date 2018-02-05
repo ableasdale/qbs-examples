b3f2ddac0ec5c06b9b11c9a0eb1ad53ac611f4c9
Alex Bleasdale <ableasdale@marklogic.com>
2018-02-05 05:18:59 +0000

    minor change

--- helloworld-qt/helloworld.cpp
+++ helloworld-qt/helloworld.cpp
@@ -3,9 +3,8 @@
 
 int main(int argc, char** argv) {
     QApplication a(argc, argv);
-    QString message = "Hello, World !!!!!!!";
+    QString message = "Hello, World";
 
-    QMessageBox::information(nullptr, "Hello World v2.0", message,
-                             QMessageBox::Yes | QMessageBox::No);
+    QMessageBox::information(nullptr, "Hello World GUI", message, QMessageBox::Yes | QMessageBox::No);
     return 0;
 }
